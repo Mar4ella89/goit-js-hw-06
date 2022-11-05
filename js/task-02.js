33f7345a6987d28590c5d6dafb88589433a26a38
Marat Baikin <3371335@gmail.com>
2022-11-06 02:22:38 +0300

    Completed task-2.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listItems = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  console.log(itemEl);
+  return itemEl;
+});
+
+const listId = document.querySelector("#ingredients");
+console.log(listId);
+
+listId.append(...listItems);
