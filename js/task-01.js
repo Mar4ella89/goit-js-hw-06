1b1abe0f6b5fd1299856c36851582ef7433afb85
Marat Baikin <3371335@gmail.com>
2022-11-06 00:19:18 +0300

    Complete task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,11 @@
-// const categories = document.querySelector("#categories");
-// console.log(categories);
-
 const menuItemsByClass = document.querySelectorAll(".item");
 console.log(menuItemsByClass);
 
 const quantityCategory = menuItemsByClass.length;
 console.log("Number of categories: ", quantityCategory);
 
-console.log(menuItemsByClass.li);
+menuItemsByClass.forEach((item) =>
+  {console.log('Category: ', item.firstElementChild.textContent)
+  console.log('Elements: ', item.lastElementChild.children.length)
+});
+
