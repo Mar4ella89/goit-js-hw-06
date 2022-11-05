124dad8191f03498a05bb2f3a83bddc0df2da2bf
Marat Baikin <3371335@gmail.com>
2022-11-05 14:39:32 +0200

    update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+// const categories = document.querySelector("#categories");
+// console.log(categories);
+
+const menuItemsByClass = document.querySelectorAll(".item");
+console.log(menuItemsByClass);
+
+const quantityCategory = menuItemsByClass.length;
+console.log("Number of categories: ", quantityCategory);
+
+console.log(menuItemsByClass.li);
