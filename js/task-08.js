75d8318b1ba6ccc043943028cbc15d30063392d4
Marat Baikin <3371335@gmail.com>
2022-11-06 21:32:21 +0300

    Completed task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,25 +1,8 @@
-// form.login-form должна быть по 
-// событию submit.
-// Если в форме есть незаполненные поля,
-//  выводи alert с предупреждением о том,
-//   что все поля должны быть заполнены.
-// Если пользователь заполнил все поля и
-//  отправил форму, собери значения полей
-//   в обьект, где имя поля будет именем 
-//   свойства, а значение поля - 
-//   значением свойства. Для доступа к 
-//   элементам формы используй свойство 
-//   elements.
-// Выведи обьект с введенными данными в 
-// консоль и очисти значения полей формы
-//  методом reset.
-
 const loginFofmEl = document.querySelector('.login-form')
-console.log(loginFofmEl);
 
 const onFormSubmit = (event) => {
 event.preventDefault();
-console.dir(event.currentTarget);
+
 const {
     elements: { email, password }
   } = event.currentTarget;
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body')
+console.log(bodyEl);
+
+const valueColorEl = document.querySelector('span.color')
+
+const changeBgColorBody = () => {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`
+  console.log(valueColorEl.textContent = getRandomHexColor());
+ }
+
+bodyEl.addEventListener('click', changeBgColorBody)
