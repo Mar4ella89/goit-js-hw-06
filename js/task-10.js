1903f00bc264c1e62b537f7a6d5e45dc651d6da5
Marat Baikin <3371335@gmail.com>
2022-11-07 02:34:37 +0200

    complete task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumberEl = document.querySelector("#controls input");
+console.dir(inputNumberEl);
+
+const btnCreateEl = document.querySelector("button[data-create]");
+console.log(btnCreateEl);
+
+const btnDestroyEl = document.querySelector("button[data-destroy]");
+console.log(btnDestroyEl);
+
+const boxEl = document.querySelector("#boxes");
+console.log(boxEl);
+
+const createBoxes = (amount) => {
+  amount = inputNumberEl.valueAsNumber;
+  let boxWidth = 30;
+  let boxHeight = 30;
+  
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = `<div style='background-color:${getRandomHexColor()}; color:red; width:${boxWidth}px;
+  height:${boxHeight}px;'></div>`;
+    boxEl.insertAdjacentHTML("beforeend", newBox);
+    boxWidth += 10;
+    boxHeight += 10;
+  }
+  console.log(boxWidth)
+};
+
+btnCreateEl.addEventListener("click", createBoxes);
+
+const destroyBoxes = () => {
+  boxEl.innerHTML = "";
+};
+
+btnDestroyEl.addEventListener("click", destroyBoxes);
